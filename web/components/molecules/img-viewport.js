d6c7291a15efd7787fbfc48ac16ca9939cf31006
Jason <47374783+JasonFang00@users.noreply.github.com>
2021-04-21 11:57:18 -0400

    img viewport

--- web/components/molecules/img-viewport.js
+++ web/components/molecules/img-viewport.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import ImageType1 from '../atoms/item-image1/item-image1';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { spacing } from '@material-ui/system';
+import Box from '@material-ui/core/Box';
 
 
 export default function Viewport(props) {
 
     const fetchImgData = () => {
-
+        return [
+            {"imageSrc": "https://placekitten.com/640/360",
+             "imageTitle" : "Title",
+             "creatorName" : "Creator"},
+             {"imageSrc": "https://placekitten.com/640/360",
+             "imageTitle" : "Title",
+             "creatorName" : "Creator"},
+             {"imageSrc": "https://placekitten.com/640/360",
+             "imageTitle" : "Title",
+             "creatorName" : "Creator"},
+             {"imageSrc": "https://placekitten.com/640/360",
+             "imageTitle" : "Title",
+             "creatorName" : "Creator"},
+             {"imageSrc": "https://placekitten.com/640/360",
+             "imageTitle" : "Title",
+             "creatorName" : "Creator"},
+             {"imageSrc": "https://placekitten.com/640/360",
+             "imageTitle" : "Title",
+             "creatorName" : "Creator"},
+             {"imageSrc": "https://placekitten.com/640/360",
+             "imageTitle" : "Title",
+             "creatorName" : "Creator"},
+             {"imageSrc": "https://placekitten.com/640/360",
+             "imageTitle" : "Title",
+             "creatorName" : "Creator"},
+             {"imageSrc": "https://placekitten.com/640/360",
+             "imageTitle" : "Title",
+             "creatorName" : "Creator"},
+             {"imageSrc": "https://placekitten.com/640/360",
+             "imageTitle" : "Title",
+             "creatorName" : "Creator"},
+        ]
     }
 
     const buildImgGrid = (imgData) => {
@@ -15,7 +49,19 @@ export default function Viewport(props) {
                 {imgData.map(
                     (data) => {
                         return (
-                            <Grid item xs={12}>
+                            <Grid item xs={3} component={Box}>
+                                
+                                <Box paddingRight={2} display="inline">
+                                    <Typography variant="h6" align="left" display="inline">
+                                        {data.imageTitle}
+                                    </Typography>
+                                </Box>
+                                <Box display="inline">
+                                    <Typography variant="subtitle1" display="inline">
+                                        {"By: " + data.creatorName}
+                                    </Typography>
+                                </Box>
+
                                 <ImageType1 src={data.imageSrc}/>
                             </Grid>
                         )
@@ -27,7 +73,9 @@ export default function Viewport(props) {
 
     return (
         <>
-            {buildImgGrid(fetchImgData())}
+            {
+                buildImgGrid(fetchImgData())
+            }
         </>
     )
 }
